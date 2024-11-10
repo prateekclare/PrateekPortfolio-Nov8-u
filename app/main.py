fd07eb030d93e171368eba779bbd03f13e80ad9e
prateek.clare <83678636+prateekclare@users.noreply.github.com>
2024-11-09 19:04:56 -0500

    Updated about and home

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home-nov8.html')
+
+@app.route('/works')
+def works():                                                                                                             
+    return render_template('h_works.html')
+
+@app.route('/works/graphics')
+def gdp():
+    return render_template('h_wo_graphics1.html')
+
+@app.route('/hire')
+def hire():
+    return render_template('h_hire.html')
+
+@app.route('/about')
+def about():
+    return render_template('h_aboutme.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
